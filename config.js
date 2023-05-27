2d3edbb9fe0e9845f244883a38413937e04bef05
Shani2222 <127343144+Shani2222@users.noreply.github.com>
2023-05-27 11:38:16 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,19 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
+ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'https://i.imgur.com/WLQqA2D.jpeg
+
+*👋𝑰 𝒂𝒎 𝑶𝒏𝒍𝒊𝒏𝒆 𝑵𝒐𝒘 🤖*
+
+𝒀𝒐𝒖 𝑪𝒂𝒏 𝑨𝒍𝒔𝒐 𝑱𝒐𝒊𝒏 𝑶𝒖𝒓 𝑮𝒓𝒐𝒖𝒑👇
+
+    𝐵𝛩𝑇 𝑊𝛨𝛥𝑇𝑆𝛥𝛲𝛲🤖
+
+https://chat.whatsapp.com/HGZLdOeJuvQ85s29pvX2iB' : process.env.ALIVE_MSG,
 LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
-ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
-FOOTER: process.env.FOOTER === undefined ? 'ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
-CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
+ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/WLQqA2D.jpeg' : process.env.ALIVE_LOGO,
+FOOTER: process.env.FOOTER === undefined ? '☠️мґ.ʊηкη◎ẘη☠️' : process.env.FOOTER,
+CAPTION: process.env.CAPTION === undefined ? ',*☛мα∂є ву мґ.ʊηкη◎ẘη☠️*' : process.env.CAPTION,
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
